26664249deb56550538a413ef3f55ac23835844e
Rubén <ruben.camarena@pucp.pe>
2020-03-17 18:48:32 -0500

    primer commit

---
+++ src/main/java/everis/bootcamp/creditproductms/CreditproductmsApplication.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.creditproductms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditproductmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditproductmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/everis/bootcamp/creditproductms/CreditproductmsApplicationTests.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.creditproductms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditproductmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
