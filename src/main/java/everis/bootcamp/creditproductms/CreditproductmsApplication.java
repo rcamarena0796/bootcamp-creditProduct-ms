08744f2c560e3e2cb8f3d469cbea245859a88b26
Rubén <ruben.camarena@pucp.pe>
2020-03-23 15:52:05 -0500

    eureka server config

--- src/main/java/everis/bootcamp/creditproductms/CreditproductmsApplication.java
+++ src/main/java/everis/bootcamp/creditproductms/CreditproductmsApplication.java
@@ -2,7 +2,10 @@ package everis.bootcamp.creditproductms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CreditproductmsApplication {
 
--- src/test/java/everis/bootcamp/creditproductms/expose/CreditproductmsControllerTest.java
+++ src/test/java/everis/bootcamp/creditproductms/expose/CreditproductmsControllerTest.java
@@ -1,18 +1,32 @@
 package everis.bootcamp.creditproductms.expose;
 
+import everis.bootcamp.creditproductms.controller.CreditProductController;
+import everis.bootcamp.creditproductms.dao.CreditProductRepository;
 import everis.bootcamp.creditproductms.model.CreditProduct;
+import everis.bootcamp.creditproductms.service.CreditProductService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+
+
 @ExtendWith(SpringExtension.class)
-@WebFluxTest
+@WebFluxTest(controllers = CreditProductController.class)
+@Import(CreditProductService.class)
 public class CreditproductmsControllerTest {
+
+    @MockBean
+    protected CreditProductService clientService;
+
+    @MockBean
+    CreditProductRepository repository;
 
     @Autowired
     private WebTestClient webClient;
